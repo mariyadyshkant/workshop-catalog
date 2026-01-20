58c8ccc15a96e7de364f3138852185ece86e64cf
mariyadyshkant <mariya.dyshkant@gmail.com>
2026-01-20 21:48:57 +0100

    Add React page to list courses from API

---
+++ workshop-catalog-react/src/App.jsx
@@ -0,0 +1,22 @@
+//import { useState } from 'react'
+import { CoursesList } from './pages/CoursesList'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <h1>Workshop Catalog</h1>
+      <CoursesList />
+      <p>
+        Edit <code>src/App.jsx</code> and save to test HMR
+      </p>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
