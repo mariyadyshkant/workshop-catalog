703c8b7e57582613499e916eba8459b011b30398
mariyadyshkant <mariya.dyshkant@gmail.com>
2026-01-24 20:00:20 +0100

    Remove unused imports and clean up App component

--- workshop-catalog-react/src/App.jsx
+++ workshop-catalog-react/src/App.jsx
@@ -1,4 +1,3 @@
-//import { useState } from 'react'
 import { CoursesList } from './pages/CoursesList'
 import './App.css'
 
@@ -9,12 +8,7 @@ function App() {
 
       <h1>Workshop Catalog</h1>
       <CoursesList />
-      <p>
-        Edit <code>src/App.jsx</code> and save to test HMR
-      </p>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
