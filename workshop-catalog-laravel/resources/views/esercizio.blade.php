837946d56b1e1916da93f97b540d3d227ada451d
mariyadyshkant <mariya.dyshkant@gmail.com>
2026-06-22 17:42:22 +0200

    Add nixpacks configuration and create esercizio view for passenger data processing

---
+++ workshop-catalog-laravel/resources/views/esercizio.blade.php
@@ -0,0 +1,41 @@
+<?php
+$passengers = [
+    [
+        'id' => 1,
+        'passengerName' => 'Freddie Mercury',
+        'isVegetarianOrVegan' => false,
+        'connectedFlights' => 2,
+    ],
+    [
+        'id' => 2,
+        'passengerName' => 'Amy Winehouse',
+        'isVegetarianOrVegan' => true,
+        'connectedFlights' => 4,
+    ],
+    [
+        'id' => 3,
+        'passengerName' => 'Kurt Cobain',
+        'isVegetarianOrVegan' => true,
+        'connectedFlights' => 3,
+    ],
+    [
+        'id' => 3,
+        'passengerName' => 'Michael Jackson',
+        'isVegetarianOrVegan' => true,
+        'connectedFlights' => 1,
+    ],
+];
+
+$passengerNames = [];
+
+foreach ($passengers as $passenger) {
+    $name = $passenger['passengerName'];
+    if ($passenger['isVegetarianOrVegan'] === true) {
+        $passengerNames[] = $name; 
+    }
+}
+
+
+var_dump($passengerNames);
+
+?>
