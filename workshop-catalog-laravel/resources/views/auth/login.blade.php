081507b92eb57c8f9ae106afdfa3b8e36d772943
mariyadyshkant <mariya.dyshkant@gmail.com>
2026-09-18 00:40:17 +0200

    Laravel: link alla registrazione nella pagina di login
    Breeze di base non lo include (il register ha già il link inverso
    verso il login). Utile visto che la registrazione resta aperta.

--- workshop-catalog-laravel/resources/views/auth/login.blade.php
+++ workshop-catalog-laravel/resources/views/auth/login.blade.php
@@ -43,5 +43,12 @@
                 {{ __('Log in') }}
             </x-primary-button>
         </div>
+
+        <p class="mt-4 text-sm text-gray-600 dark:text-gray-400">
+            {{ __("Don't have an account?") }}
+            <a class="underline hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('register') }}">
+                {{ __('Register') }}
+            </a>
+        </p>
     </form>
 </x-guest-layout>
